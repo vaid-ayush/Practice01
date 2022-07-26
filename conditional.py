6ee72a5ce92a8d057e966353669813f94fe367f3
ayush <ayush.vaid@shorthillstech.com>
2022-07-26 13:22:06 +0530

    practice example conditional logic

---
+++ conditional.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the first number"))
+b=int(input("Enter the second nmber"))
+
+if a>b:
+    print(a,"is greater than",b)
+
+elif a==b:
+    print(a,"is equal to",b)
+
+else:
+    print(b,"is greater than",a)
+
