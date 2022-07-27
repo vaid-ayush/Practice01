812ac38c73edf0bac2a49f1b2b32a1abcf754a1d
ayush <ayush.vaid@shorthillstech.com>
2022-07-28 01:32:03 +0530

    practice example variable public and private

---
+++ private_variable.py
@@ -0,0 +1,11 @@
+class Variable():
+    def __init__(self,public,private):
+        self.public=public
+        self.__private=private
+        
+
+variable= Variable("Public Variable","Private variable")
+print(variable.public)
+#print(variable.__private)    
+'''This will give error as private variable cannot be accessible outside its method'''
+print(variable._Variable__private)
