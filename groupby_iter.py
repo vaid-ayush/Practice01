98688a15656e8c0e0226e55455213af6d7e12c2b
ayush <ayush.vaid@shorthillstech.com>
2022-08-13 14:13:43 +0530

    iterating through thhe groups

---
+++ groupby_iter.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+data1 = {'Name':['Jai', 'Anuj', 'Jai', 'Princi',
+                 'Gaurav', 'Anuj', 'Princi', 'Abhi'],
+        'Age':[27, 24, 22, 32,
+               33, 36, 27, 32],
+        'Address':['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj',
+                   'Jaunpur', 'Kanpur', 'Allahabad', 'Aligarh'],
+        'Qualification':['Msc', 'MA', 'MCA', 'Phd',
+                         'B.Tech', 'B.com', 'Msc', 'MA']}
+
+df = pd.DataFrame(data1)
+print(df)                         
+
+gb = df.groupby("Name")
+for name, group in gb:
+    print(name)
+    print(group)
+    print()
