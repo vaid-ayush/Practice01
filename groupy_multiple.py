c7aa5e9fb3497227add7b563caecd21b9b0eb1b3
ayush <ayush.vaid@shorthillstech.com>
2022-08-13 14:14:12 +0530

    multiple groups iterating

---
+++ groupy_multiple.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data1 = {'Name':['Jai', 'Anuj', 'Jai', 'Princi',
+                 'Gaurav', 'Anuj', 'Princi', 'Abhi'],
+        'Age':[27, 24, 22, 32,
+               33, 36, 27, 32],
+        'Address':['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj',
+                   'Jaunpur', 'Kanpur', 'Allahabad', 'Aligarh'],
+        'Qualification':['Msc', 'MA', 'MCA', 'Phd',
+                         'B.Tech', 'B.com', 'Msc', 'MA']}
+
+df = pd.DataFrame(data1)                        
+
+gb = df.groupby(["Name","Qualification"])
+for name, group in gb:
+    print(name)
+    print(group)
+    print()
