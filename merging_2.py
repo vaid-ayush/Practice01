f5853feb49cf804ff8105f5859e069e210f3dcf6
ayush <ayush.vaid@shorthillstech.com>
2022-08-13 16:37:55 +0530

    merging two dataframes pandas

---
+++ merging_2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+data1 = {'key': ['K0', 'K1', 'K2', 'K3'],
+         'key1': ['K0', 'K1', 'K0', 'K1'],
+         'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'], 
+        'Age':[27, 24, 22, 32],} 
+   
+data2 = {'key': ['K0', 'K1', 'K2', 'K3'],
+         'key1': ['K0', 'K0', 'K0', 'K0'],
+         'Address':['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'], 
+        'Qualification':['Btech', 'B.A', 'Bcom', 'B.hons']} 
+
+df = pd.DataFrame(data1)
+df1 = pd.DataFrame(data2)
+
+print(df, "\n\n", df1)
+
+res = pd.merge(df, df1, on=["key","key1"])
+print(res)
